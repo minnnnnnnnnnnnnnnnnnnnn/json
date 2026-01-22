b5e802673cb22be8060645fce9bbd6443cd1db7e
louischo303 <louischo303@gmail.com>
2026-01-22 09:53:36 +0800

    .

--- laws/test.js
+++ laws/test.js
@@ -1,6 +1,6 @@
 $( () => 
 {
-    let lr , l ; 
+    let lr , l , f = null ; 
     fetch( new Request( "https://tcfshsu.github.io/law/json/laws.json" ) ).then( ( res ) => res.json() ).then( ( lll ) => 
     {
         lr = lll[0] ; 
@@ -31,7 +31,8 @@ $( () =>
         }
         $( "#gen" ).before() ; 
     } ) ; 
-    // should be local
+    $( "#l" ).on( "submit" , () => 
+    {
         const now = new Date() ; 
         const u = String( now.getFullYear() ).padStart( 4 , "0" ) + "/" + String( now.getMonth() + 1 ).padStart( 2 , "0" ) + "/" + String( now.getDate() ).padStart( 2 , "0" ) ; 
         console.log( u ) ; 
@@ -39,10 +40,51 @@ $( () =>
         out += "\t{\n" ; 
         out += "\t\t\"UpdateDate\": \"" + u + "\",\n" ; 
         out += "\t\t\"Laws\": [\n" ; 
-        // laws
+        for( let a of l ) 
+        {
+            if( $( "#" + a.LawURL.replace( "https://tcfshsu.github.io/law/laws/law?a=" , "" ) ).is( ":checked" ) ) 
+            {
+                $
+            }
+            else 
+            {
+                out += "\t\t\t\t\"LawLevel\": \"" + a.LawLevel.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawName\": \"" + a.LawName.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawURL\": \"" + a.LawURL.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawCategory\": \"" + a.LawCategory.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawModifiedDate\": \"" + a.LawModifiedDate.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawEffectiveDate\": \"" + a.LawEffectiveDate.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawEffectiveNote\": \"" + a.LawEffectiveNote.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawAbandonNote\": \"" + a.LawAbandonNote.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawHasEngVersion\": \"" + a.LawHasEngVersion.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"EngLawName\": \"" + a.EngLawName.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawAttachments\": [" ; 
+                out += "], \n" ; 
+                out += "\t\t\t\t\"LawHistories\": \"" + a.LawHistories.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawForeword\": \"" + a.LawForeword.replaceAll( "\r\n" , "\\r\\n" ) + "\", \n" ; 
+                out += "\t\t\t\t\"LawArticles\": [" ; 
+                out += "], \n" ; 
+            }
+        }
         out += "\t\t] \n" ; 
         out += "\t} \n" ; 
         out += "] " ; 
         out = out.replaceAll( "\t" , "    " ) ; 
-    // should be local
+        console.log( out ) ; 
+        if( f !== null ) 
+        {
+            window.URL.revokeObjectURL( f ) ; 
+        }
+        f = window.URL.createObjectURL( new Blob( [ out ] , { type : "application/json" } ) ) ; 
+        if( $( "#dl" ).length ) 
+        {
+            $( "#dl" ).remove() ; 
+        }
+        $( "<div />" , 
+        {
+            id: "dl", 
+            append: "<a href=\"" + f + "\" download=\"laws.json\">下載</a>", 
+            appendTo: "main" 
+        } ) ; 
+    } ) ; 
 } ) ; 
