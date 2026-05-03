ea3c6938e9acf1b1afba0f74129b89387cd5655f
louischo303 <louischo303@gmail.com>
2026-05-03 22:24:36 +0800

    ...

--- laws/test.js
+++ laws/test.js
@@ -1,6 +1,6 @@
 $( () => 
 {
-    let fetch_done = false , lr , l , f = null , /* fl = null , */ domain = "https://tcfshsu.github.io/law" , aNote , first , att_i , ai = 0 , li = 0 , la , lfirst , l_att_i , n_art ; 
+    let fetch_done = false , lr , l , f = null , /* fl = null , */ domain = "https://tcfshsu.github.io/law" , aNote , first , att_i , ai = 0 , li = 0 , la = Array() , lfirst = Array() , l_att_i = Array() , n_art = Array() ; 
     const ch_num_lt_ten = "○一二三四五六七八九十" ; 
     const ch_num = { ...ch_num_lt_ten , 11: ch_num_lt_ten[10] + ch_num_lt_ten[1] , 12: ch_num_lt_ten[10] + ch_num_lt_ten[2] , 13: ch_num_lt_ten[10] + ch_num_lt_ten[3] , 14: ch_num_lt_ten[10] + ch_num_lt_ten[4] , 15: ch_num_lt_ten[10] + ch_num_lt_ten[5] , 16: ch_num_lt_ten[10] + ch_num_lt_ten[6] , 17: ch_num_lt_ten[10] + ch_num_lt_ten[7] , 18: ch_num_lt_ten[10] + ch_num_lt_ten[8] , 19: ch_num_lt_ten[10] + ch_num_lt_ten[9] , 100: "百" , 110: "百一十" } ; 
     const ch_cap_num_lt_ten = "零壹貳參肆伍陸柒捌玖拾" ; 
@@ -13,10 +13,6 @@ $( () =>
         aNote = Array( l.length ).fill( false ) ; 
         first = Array( l.length ).fill( true ) ; 
         att_i = Array( l.length ).fill( 0 ) ; 
-        la = Array() ; 
-        lfirst = Array() ; 
-        l_att_i = Array() ; 
-        n_art = Array() ; 
         for( let a of l ) 
         {
             const i = ai ; 
@@ -986,6 +982,7 @@ $( () =>
         out += "] " ; 
         out = out.replaceAll( "\t" , "    " ) ; 
         console.log( out ) ; 
+        $( "#out" ).text( out ) ; 
         if( f !== null ) 
         {
             window.URL.revokeObjectURL( f ) ; 
@@ -995,12 +992,7 @@ $( () =>
         {
             $( "#dl" ).remove() ; 
         }
-        $( "<div />" , 
-        {
-            id: "dl", 
-            append: "<a href=\"" + f + "\" download=\"laws.json\">下載laws.json</a>", 
-            appendTo: "main" 
-        } ) ; 
+        $( "#preview" ).before( $( "<div />" , { id: "dl" , append: "<a href=\"" + f + "\" download=\"laws.json\">下載laws.json</a>" } ) ) ; 
         // ---latest.json-----------------
         // let lout = "[" ; 
         // lout += "\t{" ; 
